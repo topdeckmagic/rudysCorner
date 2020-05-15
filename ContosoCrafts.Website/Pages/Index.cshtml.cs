1c628bc61ecfe702e35eea51dc8326d356c1f873
Colin Aylward <caylward@seattleu.edu>
2020-05-14 20:17:48 -0700

    first commit

---
+++ ContosoCrafts.Website/Pages/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using ContosoCrafts.Website.Models;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace ContosoCrafts.WebSite.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger,
+            JsonFileProductService productService)
+        {
+            _logger = logger;
+            ProductService = productService;
+        }
+
+        public JsonFileProductService ProductService { get; }
+        public IEnumerable<Product> Products { get; private set; }
+        
+        public void OnGet()
+        {
+            Products = ProductService.GetProducts();
+        }
+    }
+}
