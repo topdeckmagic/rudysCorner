6bf9c0a589ac1a028e1859fc6acaac33a9e193fb
daniellemiller <dbeaulieu@seattleu.edu>
2020-05-20 21:03:43 -0700

    Add Edit POI Page

---
+++ RudysCorner.Website/Pages/EditPOI.cshtml.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RudysCorner.Website.Pages
+{
+    public class EditPOIModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
