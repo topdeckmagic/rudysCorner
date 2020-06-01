8b0a798fef17adda5b993e1211cceda68311a3b2
Colin Aylward <caylward@seattleu.edu>
2020-05-31 20:53:56 -0700

    Added comments per feedback to site.js.  Related Card: https://tasks.office.com/redhawks.onmicrosoft.com/Home/Task/86Tvl88VPUy20SySqy4pPmQAExwJ?Type=TaskLink&Channel=Link&CreatedTime=637265804014220000

--- RudysCorner.Website/wwwroot/js/site.js
+++ RudysCorner.Website/wwwroot/js/site.js
@@ -2,14 +2,18 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+//allowDrop() specifies where a dragable item can be dropped  
 function allowDrop(ev) {
     ev.preventDefault();
 }
 
+//drag() specifies what is being dragged
 function drag(ev) {
     ev.dataTransfer.setData("text", ev.target.id);
 }
 
+//drop() when a dragged item is dropped this happens
 function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
