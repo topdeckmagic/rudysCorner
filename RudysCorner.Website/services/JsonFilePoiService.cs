c043400a4ea990977a75d00049dd2340300e28b2
Colin Aylward <caylward@seattleu.edu>
2020-06-02 15:31:51 -0700

    Created JsonFilePoiService.cs Related Card: https://tasks.office.com/redhawks.onmicrosoft.com/Home/Task/Mquh1ueP606k3mDJE98Wv2QAFInB?Type=TaskLink&Channel=Link&CreatedTime=637267338951670000

---
+++ RudysCorner.Website/services/JsonFilePoiService.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json;
+using ContosoCrafts.Website.Models;
+using Microsoft.AspNetCore.Hosting;
+using RudysCorner.Website.Models;
+
+namespace RudysCorner.Website.services
+{
+    public class JsonFilePoiService
+    {
+        public JsonFilePoiService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironmnet = webHostEnvironment;
+        }
+
+        public IWebHostEnvironment WebHostEnvironmnet { get; }
+        
+        private string JsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironmnet.WebRootPath, "data", "pois.json"); }
+        }
+        public IEnumerable<Poi> GetPois()
+        {
+            using (var jsonFileReader = File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<Poi[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+            }
+        }
+
+        public Poi GetPoi(string poiId)
+        {
+            var pois = GetPois();
+            return pois.First(x => x.Id == poiId);
+        }
+    }
+}
