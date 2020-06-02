d4b830bc3baa64fc32e3c8e4958e7caa15f4a62d
Colin Aylward <caylward@seattleu.edu>
2020-06-02 15:04:25 -0700

    Created Poi.cs

---
+++ RudysCorner.Website/Models/Poi.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace RudysCorner.Website.Models
+{
+    public class Poi
+    {
+        public string Id { get; set; }
+
+        [JsonPropertyName("img")]
+        public string Image { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string[] FAQ { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize(this);
+    }
+}
