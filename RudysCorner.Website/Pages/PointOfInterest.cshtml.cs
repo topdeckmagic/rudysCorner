c8a54dd9e678f12ba70efb93014717dba07d1f38
Colin Aylward <caylward@seattleu.edu>
2020-06-02 16:14:56 -0700

    Adding GetPoi to PointOfInterst.cshtml Related Card: https://tasks.office.com/redhawks.onmicrosoft.com/Home/Task/Mquh1ueP606k3mDJE98Wv2QAFInB?Type=TaskLink&Channel=Link&CreatedTime=637267364892160000

--- RudysCorner.Website/Pages/PointOfInterest.cshtml.cs
+++ RudysCorner.Website/Pages/PointOfInterest.cshtml.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using RudysCorner.Website.Models;
+using RudysCorner.Website.services;
 
 namespace RudysCorner.Website.Pages
 {
     public class PointOfInterestModel : PageModel
     {
-        public void OnGet()
+        public string Id { get; set; }
+        public Poi Poi { get; private set;}
+        public JsonFilePoiService PoiService { get; }
+        public PointOfInterestModel(JsonFilePoiService poiService)
         {
-
+            PoiService = poiService;
+        }
+        public void OnGet(string id)
+        {
+            Id = id;
+            Poi = PoiService.GetPoi(Id);
         }
     }
 }
