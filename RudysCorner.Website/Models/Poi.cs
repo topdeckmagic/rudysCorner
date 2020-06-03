81c20312496370a65c47e33ba66b0d64cf75d3ed
Colin Aylward <caylward@seattleu.edu>
2020-06-03 16:14:40 -0700

    Adding NextStop to POI Data Model Poi.cs + pois.json

--- RudysCorner.Website/Models/Poi.cs
+++ RudysCorner.Website/Models/Poi.cs
@@ -16,6 +16,7 @@ namespace RudysCorner.Website.Models
         public string Title { get; set; }
         public string Description { get; set; }
         public string LongDescription { get; set; }
+        public string NextStop { get; set; }
         public string[] FAQ { get; set; }
 
         public override string ToString() => JsonSerializer.Serialize(this);
