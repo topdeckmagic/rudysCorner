e44e79273184b442f11532e972fbb8b7d4432d09
Colin Aylward <caylward@seattleu.edu>
2020-06-03 16:12:42 -0700

    Added LongDescrtipion to POI Datastructure Poi.cs + pois.json

--- RudysCorner.Website/Models/Poi.cs
+++ RudysCorner.Website/Models/Poi.cs
@@ -15,6 +15,7 @@ namespace RudysCorner.Website.Models
         public string Image { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+        public string LongDescription { get; set; }
         public string[] FAQ { get; set; }
 
         public override string ToString() => JsonSerializer.Serialize(this);
