98ef1dfc8f0daa50c4665d9633d9e8cd6dd54e19
Colin Aylward <caylward@seattleu.edu>
2020-06-03 18:18:50 -0700

    Adding GetPois to Analytics.cs

--- RudysCorner.Website/Pages/Analytics.cshtml.cs
+++ RudysCorner.Website/Pages/Analytics.cshtml.cs
@@ -4,14 +4,22 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RudysCorner.Website.Models;
+using RudysCorner.Website.services;
 
 namespace RudysCorner.Website.Pages
 {
     public class AnalyticsModel : PageModel
     {
+        public IEnumerable<Poi> Pois;
+        public JsonFilePoiService PoiService { get; }
+        public AnalyticsModel(JsonFilePoiService poiService)
+        {
+            PoiService = poiService;
+        }
         public void OnGet()
         {
-
+            Pois = PoiService.GetPois();
         }
     }
 }
